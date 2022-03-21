9c90bbe8b0d393a70bfafdaacf7fbb74c0ab8136
Ali Fertah <85651433+alifertah@users.noreply.github.com>
2022-03-21 15:47:13 +0100

    Add files via upload

---
+++ mini talk/client.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfertah <alfertah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/19 21:55:13 by alfertah          #+#    #+#             */
+/*   Updated: 2022/03/16 20:04:17 by alfertah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	send_msg(char *msg, int pid)
+{
+	int	i;
+	int	shift;
+
+	i = -1;
+	while (++i < ft_strlen(msg))
+	{
+		shift = -1;
+		while (++shift < 8)
+		{
+			if (msg[i] & (128 >> shift))
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					exit(1);
+			}
+			else
+			{
+				if (kill(pid, SIGUSR2) == -1)
+					exit(1);
+			}
+			usleep(800);
+		}
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		ppid;
+
+	if (argc != 3)
+	{
+		ft_putstr("ta chkaddir!");
+		exit(1);
+	}
+	ppid = ft_atoi(argv[1]);
+	if (ppid == -1)
+		exit(1);
+	send_msg(argv[2], ppid);
+}
